c004121f73627e0403fb3a86b5b5de5514316fb9
Death <lkwq007@gmail.com>
2015-11-15 23:16:12 +0800

    Add 1058

---
+++ 1058-A+B-in-Hogwarts.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+int main(void)
+{
+	long long galleon_P,sickle_P,knut_P,galleon_A,sickle_A,knut_A,galleon,sickle,knut,flag;
+	scanf("%lld.%lld.%lld %lld.%lld.%lld",&galleon_P,&sickle_P,&knut_P,&galleon_A,&sickle_A,&knut_A);
+	knut_P=knut_P+galleon_P*17*29+sickle_P*29;
+	knut_A=knut_A+galleon_A*17*29+sickle_A*29;
+	knut_A+=knut_P;
+	knut=knut_A%29;
+	sickle_A=knut_A/29;
+	sickle=sickle_A%17;
+	galleon_A=sickle_A/17;
+	galleon=galleon_A;
+	printf("%lld.%lld.%lld\n",galleon,sickle,knut);
+	return 0;
+}
