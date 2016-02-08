031bf75e04f43f7dfebb0947223820128aca4cf8
Death <lkwq007@gmail.com>
2016-02-08 17:11:32 +0800

    Update 1052

--- 1052-Linked-List-Sorting.c
+++ 1052-Linked-List-Sorting.c
@@ -1,50 +1,78 @@
 #include<stdio.h>
+#include<stdlib.h>
 typedef struct list_
 {
+	int address;
 	int key;
 	int next;
+	int in_list;
 } list;
-list data[100001];
+int memory[100000];
+int compare(const void *a,const void *b)
+{
+	if(((list *)b)->in_list>((list *)a)->in_list)
+	{
+		return -1;
+	}
+	else if(((list *)b)->in_list<((list *)a)->in_list)
+	{
+		return 1;
+	}
+	return ((list *)a)->key-((list *)b)->key;
+}
 int main()
 {
-	int n,i,head,pre,temp,j,address,dummy;
+	int n,head,i,address,count=0,pre=-1;
+	list *data;
 	scanf("%d%d",&n,&head);
-	dummy=100000;
-	data[dummy].next=head;
+	data=(list *)malloc(n*sizeof(list));
 	for(i=0;i<n;i++)
 	{
-		scanf("%d",&address);
-		scanf("%d%d",&(data[address].key),&(data[address].next));
+		scanf("%d%d%d",&(data[i].address),&(data[i].key),&(data[i].next));
+		memory[data[i].address]=i;
+		data[i].in_list=0;
 	}
-	pre=dummy;
-	for(i=head;data[i].next!=-1;i=data[i].next)
-	{
-		for(j=i;data[j].next!=-1;j=data[j].next)
-		{
-			if(data[j].key>data[data[j].next].key)
-			{
-				data[pre].next=data[j].next;
-				temp=data[data[j].next].next;
-				data[data[j].next].next=j;
-				data[j].next=temp;
-			}
-			pre=j;
-			printf("%d\n",i);
-		}
-		pre=i;
-	}
-	address=data[dummy].next;
+	address=head;
 	while(address!=-1)
 	{
-		printf("%05d %d ",address,data[address].key);
-		if(data[address].next!=-1)
+		data[memory[address]].in_list=1;
+		address=data[memory[address]].next;
+	}
+	qsort(data,n,sizeof(list),compare);
+	head=-1;
+	for(i=0;i<n;i++)
+	{
+		if(data[i].in_list==1)
 		{
-			printf("%05d\n",data[address].next);
+			if(head==-1)
+			{
+				head=data[i].address;
+			}
+			count++;
 		}
-		else
+	}
+	if(head!=-1)
+	{
+		printf("%d %05d\n",count,head);
+	}
+	else
+	{
+		printf("%d %d\n",count,head);
+	}
+	for(i=0;i<n;i++)
+	{
+		if(data[i].in_list==1)
 		{
-			printf("-1\n");
+			if(pre!=-1)
+			{
+				printf("%05d\n",data[i].address);
+			}
+			printf("%05d %d ",data[i].address,data[i].key);
+			pre=data[i].address;
 		}
-		address=data[address].next;
+	}
+	if(pre!=-1)
+	{
+		printf("-1\n");
 	}
 }
